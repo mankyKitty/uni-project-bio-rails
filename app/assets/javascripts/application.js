a2d9a40fd0583afb9932a43f7f39fcde05742091
Sean Chalmers <sean_chalmers@flightcentre.com.au>
2013-05-11 21:42:44 +1000

    Changed to CDN Hosting
    Moved to Google Content Delivery Network for Angular.js and jQuery.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require angular
 //= require angular-resource
-//= require jquery
 //= require bootstrap-transition
 //= require bootstrap-tab
 //= require bootstrap-collapse
